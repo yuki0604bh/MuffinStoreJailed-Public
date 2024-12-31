6f377aa3d58686804fab08dc5a36bc9ae20763be
Mineek <84083936+mineek@users.noreply.github.com>
2024-12-31 14:18:11 +0100

    Initial Commit

---
+++ MuffinStoreJailed/MuffinStoreJailedApp.swift
@@ -0,0 +1,17 @@
+//
+//  MuffinStoreJailedApp.swift
+//  MuffinStoreJailed
+//
+//  Created by Mineek on 31/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct MuffinStoreJailedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
